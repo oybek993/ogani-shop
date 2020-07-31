ef5db7433b8a9ea86cb44f02023a40a438f05aba
oybek993 <melikulov990@gmail.com>
2020-07-31 12:07:37 +0500

    Updates

---
+++ precache-manifest.8fcbd9bc5570fae32a6b3b8888f592d1.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a080aa68162eb3b872dcac1f1b340de",
+    "url": "/ogani-shop/index.html"
+  },
+  {
+    "revision": "444412ccd049e3093289",
+    "url": "/ogani-shop/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "362ae2ab57750f9ce368",
+    "url": "/ogani-shop/static/css/main.7c7b9b88.chunk.css"
+  },
+  {
+    "revision": "444412ccd049e3093289",
+    "url": "/ogani-shop/static/js/2.4bbdbe40.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ogani-shop/static/js/2.4bbdbe40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "362ae2ab57750f9ce368",
+    "url": "/ogani-shop/static/js/main.e28a03e8.chunk.js"
+  },
+  {
+    "revision": "f8c64b7870931299b738",
+    "url": "/ogani-shop/static/js/runtime-main.0e5e490f.js"
+  },
+  {
+    "revision": "3dfe7510175d30288bd49978c661ff72",
+    "url": "/ogani-shop/static/media/banner-1.3dfe7510.jpg"
+  },
+  {
+    "revision": "19df76e23a8bd988598a378134b41a35",
+    "url": "/ogani-shop/static/media/banner-2.19df76e2.jpg"
+  },
+  {
+    "revision": "ee3b830a454f2f50c01b4951c378bdba",
+    "url": "/ogani-shop/static/media/banner.ee3b830a.jpg"
+  },
+  {
+    "revision": "2e200d5a5f77d3ef248d7cdc28530a75",
+    "url": "/ogani-shop/static/media/blog-1.2e200d5a.jpg"
+  },
+  {
+    "revision": "4a8e8218f4b8dc944d1598e8d84d8624",
+    "url": "/ogani-shop/static/media/blog-2.4a8e8218.jpg"
+  },
+  {
+    "revision": "4183427ac1de27c08d03d844ffefa407",
+    "url": "/ogani-shop/static/media/blog-3.4183427a.jpg"
+  },
+  {
+    "revision": "ede54a5b5693de9582fe67b336ed29c6",
+    "url": "/ogani-shop/static/media/product-1.ede54a5b.jpg"
+  },
+  {
+    "revision": "eebc4b18f5a91dde9005e13d9766bfdc",
+    "url": "/ogani-shop/static/media/product-2.eebc4b18.jpg"
+  },
+  {
+    "revision": "ff13436ee17d9d3d586dfc62a79cffbd",
+    "url": "/ogani-shop/static/media/product-3.ff13436e.jpg"
+  },
+  {
+    "revision": "6fe6638289752ede6e098a45ac7a3673",
+    "url": "/ogani-shop/static/media/product-4.6fe66382.jpg"
+  },
+  {
+    "revision": "b0d85e918115f2bf2a58850520c7a217",
+    "url": "/ogani-shop/static/media/product-7.b0d85e91.jpg"
+  },
+  {
+    "revision": "2a0cf869dd4699c99080e79384bde903",
+    "url": "/ogani-shop/static/media/product-8.2a0cf869.jpg"
+  }
+]);
